603b9c91d18c250cb929145a307d97428d8163ef
Kriss <c.rivas@globant.com>
2020-04-20 01:08:43 -0500

    ADD_REDUX Add redux structure for app

--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button as Option } from "react-bootstrap";
 
-function Button({ label, onClickAction, variant }) {
+const Button = ({ label, onClickAction, variant }) => {
   return (
     <Option variant={variant} onClick={onClickAction}>
       {label}
--- src/components/molecules/Loading.js
+++ src/components/molecules/Loading.js
@@ -1,16 +1,28 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import styled from "styled-components";
+import { Row, Col } from "react-bootstrap";
 
 import Map from "../../assets/img/map.gif";
 
+const Container = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+
+  img {
+    position: absolute;
+    top: calc(50% - 30px);
+  }
+`;
+
 function Loading() {
   return (
-    <Container fluid>
-      <Row>
-        <Col>
-          <img src={Map} alt="Loading Map" />
-        </Col>
-      </Row>
+    <Container>
+      <img src={Map} alt="Loading Map" />
     </Container>
   );
 }
